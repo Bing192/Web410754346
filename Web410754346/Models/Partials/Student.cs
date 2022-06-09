d942246854c7425c9476c98f7c7c780d8fea99a7
陳柄菽 <ruby090215@gmail.com>
2022-06-09 20:14:19 +0800

    06/09

--- Web410754346/Models/Partials/Student.cs
+++ Web410754346/Models/Partials/Student.cs
@@ -19,7 +19,7 @@ namespace Web410754346.Models
         [Required(ErrorMessage ="請填寫姓名")]
         [Display(Name = "姓名")]
         [StringLength(5, MinimumLength = 2, ErrorMessage = "請輸入2-5個字")]
-        public string Name { get; set; }
+        public string Username { get; set; }
 
 
         [Required(ErrorMessage = "請填寫學號")]
@@ -32,5 +32,10 @@ namespace Web410754346.Models
         [Display(Name = "Email")]
         [EmailAddress]
         public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "必填欄位")]
+        [Display(Name = "是否註冊")]
+        public bool IsActive { get; set; }
     }
 }
